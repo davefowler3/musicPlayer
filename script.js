5b89c192648279164edbd1ba6a63f5d0327b7604
davefowler3 <davejf3@gmail.com>
2021-06-17 07:04:22 -0400

    Add progress bar functionality.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -69,7 +71,6 @@ function prevSong() {
     if (songIndex < 0) {
         songIndex = songs.length -1;
     }
-    console.log(songIndex);
     loadSong(songs[songIndex]);
     playSong();
 }
@@ -80,7 +81,6 @@ function nextSong() {
     if (songIndex > songs.length - 1) {
         songIndex = 0;
     }
-    console.log(songIndex);
     loadSong(songs[songIndex]);
     playSong();
 }
@@ -88,6 +88,17 @@ function nextSong() {
 // On Load - Select First Song
 loadSong(songs[songIndex]);
 
+// Update Progress Bar & Time
+function updateProgressBar(e) {
+    if (isPlaying) {
+        const { duration, currentTime } = e.srcElement;
+        // Update progess bar width
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+    }
+}
+
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
